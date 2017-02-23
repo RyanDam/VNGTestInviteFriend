c8d18108b97c84d482e78719de837c0c8312725c
RyanDam <py.ryan.dam@gmail.com>
2017-02-24 00:45:15 +0700

    Add Thumbnail creator engine

--- Contact Selector/MainViewController.m
+++ Contact Selector/MainViewController.m
@@ -12,10 +12,13 @@
 #import "CSContactBusiness.h"
 #import "CSPresenterViewController.h"
 
+#import "CSThumbnailCreater.h"
+
 @interface MainViewController () <CSViewControllerDelegate, CSViewControllerDataSource>
 
 @property (nonatomic) CSContactBusiness * contactBusiness;
 @property (nonatomic) CSContactProvider * contactProvider;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -26,6 +29,17 @@
     
     self.contactBusiness = [[CSContactBusiness alloc] init];
     self.contactProvider = [[CSContactProvider alloc] init];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    CGSize size = CGSizeMake(42, 42);
+    
+    UIImage * img = [[CSThumbnailCreater getInstance] getThumbnailImageWithText:@"AA" withSize:size];
+    
+    [self.imageView setImage:img];
+    
 }
 
 - (void)didReceiveMemoryWarning {
