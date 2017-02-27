38ea441b0ff181b1a179191590bdb58112724bed
ngocphong <ttphong2608@gmail.com>
2017-02-27 15:58:54 +0700

    [update] UI

--- Contact Selector/CallViewController.m
+++ Contact Selector/CallViewController.m
@@ -8,7 +8,21 @@
 
 #import "CallViewController.h"
 
-@interface CallViewController ()
+@interface CallViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIView *diallerView;
+@property (weak, nonatomic) IBOutlet UIView *keypadView;
+
+
+
+
+@property (weak, nonatomic) IBOutlet UIButton *addContactButton;
+@property (weak, nonatomic) IBOutlet UIButton *backspaceButton;
+@property (weak, nonatomic) IBOutlet UITextField *inputNumber;
+@property (weak, nonatomic) IBOutlet UIView *inputView;
+
+@property (strong, nonatomic) NSArray *history;
 
 @end
 
@@ -23,11 +37,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.tableView setDataSource:self];
+    [self.tableView setDelegate:self];
+    
+    self.history = @[@1, @2, @3, @4, @5, @6];
+    self.inputNumber.userInteractionEnabled = NO;
+    
+    [self.diallerView setHidden:YES];
+    [self.inputView setHidden:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showDialler:(id)sender {
+    [self.diallerView setHidden:NO];
+    [self.inputView setHidden:NO];
+    
+}
+
+- (IBAction)hideDialler:(id)sender {
+    [self.diallerView setHidden:YES];
+    [self.inputView setHidden:YES];
+}
+
+
+#pragma mark - Table View Datasource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.history count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *indentifier = @"calledCell";
+    
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:indentifier];
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 50;
 }
 
 /*
