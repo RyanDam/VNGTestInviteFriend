36129b2cdd158d053b7fb5b3045eef0a8a2c53b0
ngocphong <ttphong2608@gmail.com>
2017-02-27 18:15:54 +0700

    [add] dialler animation

--- Contact Selector/CallViewController.m
+++ Contact Selector/CallViewController.m
@@ -22,6 +22,8 @@
 @property (weak, nonatomic) IBOutlet UITextField *inputNumber;
 @property (weak, nonatomic) IBOutlet UIView *inputView;
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *diallerConstraint;
+
 @property (strong, nonatomic) NSArray *history;
 
 @end
@@ -43,8 +45,8 @@
     self.history = @[@1, @2, @3, @4, @5, @6];
     self.inputNumber.userInteractionEnabled = NO;
     
-    [self.diallerView setHidden:YES];
-    [self.inputView setHidden:YES];
+    //[self.diallerView setHidden:YES];
+    //[self.inputView setHidden:YES];
     
 }
 
@@ -53,15 +55,39 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)showDialler:(id)sender {
-    [self.diallerView setHidden:NO];
-    [self.inputView setHidden:NO];
+- (IBAction)numberClick:(id)sender {
     
+//    self.abc.constant = -300;
+    
+//    [UIView animateWithDuration:0.7 delay:0.0 usingSpringWithDamping:0.8 initialSpringVelocity:0.9 options:UIViewAnimationOptionCurveEaseOut animations:^{
+//        [self.view layoutIfNeeded];
+//    } completion:^(BOOL finished) {
+//       //
+//    }];
+    
+//    [UIView animateWithDuration:1.0 animations:^{
+//        [self.view layoutIfNeeded];
+//    }];
+    
+    
+//    [UIView animateWithDuration:0.5 animations:^{
+//        CGRect rect = self.inputView.frame;
+//        self.inputView.frame = CGRectMake(rect.origin.x, rect.origin.y + rect.size.height, rect.size.width, rect.size.height);
+//    }];
+}
+
+- (IBAction)showDialler:(id)sender {
+    self.diallerConstraint.constant = 0;
+    [UIView animateWithDuration:0.5 animations:^{
+        [self.view layoutIfNeeded];
+    }];
 }
 
 - (IBAction)hideDialler:(id)sender {
-    [self.diallerView setHidden:YES];
-    [self.inputView setHidden:YES];
+    self.diallerConstraint.constant = -400;
+    [UIView animateWithDuration:0.5 animations:^{
+        [self.view layoutIfNeeded];
+    }];
 }
 
 
